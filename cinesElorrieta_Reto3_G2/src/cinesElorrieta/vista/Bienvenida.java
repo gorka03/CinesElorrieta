03e044f168df98a0f095a53c6f097a2a7846be31
in1dm3-v <in1dm3-v@HZ317735.elorrieta.int>
2024-01-31 21:23:17 +0100

    panel bienvevida

--- cinesElorrieta_Reto3_G2/src/cinesElorrieta/vista/Bienvenida.java
+++ cinesElorrieta_Reto3_G2/src/cinesElorrieta/vista/Bienvenida.java
@@ -2,9 +2,13 @@ package cinesElorrieta.vista;
 
 import java.awt.EventQueue;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.JLabel;
 
 public class Bienvenida extends JFrame {
 
@@ -32,22 +36,35 @@ public class Bienvenida extends JFrame {
 	 */
 	public Bienvenida() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 666, 430);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JPanel panelBienvenida = new JPanel();
+		panelBienvenida.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				panelBienvenida.setVisible(false);
+				//panelLogin.setVisible(true);
+				//esperar();
+			}
+		});
+		panelBienvenida.setBounds(0, 0, 650, 391);
+		
+		contentPane.add(panelBienvenida);
+		panelBienvenida.setLayout(null);
+		
+		JLabel lblBienvenida = new JLabel("");
+		lblBienvenida.setIcon(new ImageIcon("C:\\Users\\in1dm3-v\\eclipse-workspace\\retos\\src\\reto2\\cine-logo2.png"));
+		lblBienvenida.setBounds(0, 5, 650, 386);
+		panelBienvenida.add(lblBienvenida);
 	}
 	
 	
 	public void initialize () {
 		
 	}
-	
-	
-	
-	
-	
-	
-
 }
