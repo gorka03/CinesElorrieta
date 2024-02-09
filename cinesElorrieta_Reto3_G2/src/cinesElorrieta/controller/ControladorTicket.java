e3fbacca6809b9aca437e2a3f37d788619881b99
in1dm3-v <in1dm3-v@HZ317735.elorrieta.int>
2024-02-09 17:19:07 +0100

    controlador ticket

---
+++ cinesElorrieta_Reto3_G2/src/cinesElorrieta/controller/ControladorTicket.java
@@ -0,0 +1,23 @@
+package cinesElorrieta.controller;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+import cinesElorrieta.modelo.Sesion;
+
+public class ControladorTicket {
+	
+	private static final String NOMBRE_FICHERO = "ejemplo.txt";
+	private static final String RUTA_FICHERO = "C://Users//in1dm3-v//Desktop//hola";
+
+	public void generarTicket(ArrayList<Sesion> sesiones) {
+		
+	}
+	
+}
