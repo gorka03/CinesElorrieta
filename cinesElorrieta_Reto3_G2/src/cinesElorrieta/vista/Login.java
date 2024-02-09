2be0c4dbc1ef6302a8ec252f2510b3ea17628de5
in1dm3-v <in1dm3-v@HZ327377.elorrieta.int>
2024-02-09 15:58:19 +0100

    Login con conexion a base de datos

--- cinesElorrieta_Reto3_G2/src/cinesElorrieta/vista/Login.java
+++ cinesElorrieta_Reto3_G2/src/cinesElorrieta/vista/Login.java
@@ -4,10 +4,17 @@ import java.awt.Color;
 
 import javax.swing.JPanel;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
+import cinesElorrieta.modelo.Cliente;
+import cinesElorrieta.utils.DBUtils;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.awt.event.ActionEvent;
 import java.awt.Font;
@@ -44,15 +51,43 @@ public class Login {
 		JButton btnLogin = new JButton("Login");
 		btnLogin.setBounds(288, 284, 140, 30);
 		btnLogin.setFont(new Font("Times New Roman", Font.BOLD, 12));
+
 		btnLogin.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				paneles.get(0).setVisible(false);
-				paneles.get(1).setVisible(false);
-				paneles.get(2).setVisible(false);
-				paneles.get(3).setVisible(false);
+				String usuario = textField.getText();
+				String contrasenna = new String(passwordField.getPassword());
 
+				// Verificar las credenciales en la base de datos
+				boolean credencialesValidas = verificarCredenciales(usuario, contrasenna);
+
+				if (credencialesValidas) {
+					JOptionPane.showMessageDialog(panel, "Has inciado sesión correctamente ",
+							"Inicio de sesión correcto", JOptionPane.INFORMATION_MESSAGE);
+
+					paneles.get(0).setVisible(false);
+					paneles.get(1).setVisible(true);
+					paneles.get(2).setVisible(false);
+					paneles.get(3).setVisible(false);
+				} else {
+					JOptionPane.showMessageDialog(panel, "Usuario o contraseña incorrecta", "Error de inicio de sesión",
+							JOptionPane.ERROR_MESSAGE);
+				}
 			}
+
+			private boolean verificarCredenciales(String usuario, String contrasenna) {
+				ArrayList<Cliente> clientes = obtenerDatosLogin();
+				if (clientes != null) {
+					for (Cliente cliente : clientes) {
+						if (cliente.getUserName().equals(usuario) && cliente.getContrasenna().equals(contrasenna)) {
+							return true; // Credenciales válidas
+						}
+					}
+				}
+				return false; // Credenciales inválidas
+			}
+
 		});
+
 		panel.add(btnLogin);
 
 		JButton btnRegistro = new JButton("Registrate !");
@@ -92,5 +127,82 @@ public class Login {
 
 	public JPanel getPanel() {
 		return panel;
+	}
+
+	private ArrayList<Cliente> obtenerDatosLogin() {
+		ArrayList<Cliente> ret = null;
+
+		// SQL que queremos lanzar
+		String sql = "select * from cliente";
+
+		// La conexion con BBDD
+		Connection connection = null;
+
+		// Vamos a lanzar una sentencia SQL contra la BBDD
+		// Result set va a contener todo lo que devuelve la BBDD
+		java.sql.Statement statement = null;
+		ResultSet resultSet = null;
+
+		try {
+			// El Driver que vamos a usar
+			Class.forName(DBUtils.DRIVER);
+
+			// Abrimos la conexion con BBDD
+			connection = DriverManager.getConnection(DBUtils.URL, DBUtils.USER, DBUtils.PASS);
+
+			// Vamos a lanzar la sentencia...
+			statement = connection.createStatement();
+			resultSet = statement.executeQuery(sql);
+
+			// No es posible saber cuantas cosas nos ha devuelto el resultSet.
+			// Hay que ir 1 por 1 y guardandolo todo en su objeto Ejemplo correspondiente
+			while (resultSet.next()) {
+
+				// Si es necesario, inicializamos la lista
+				if (null == ret)
+					ret = new ArrayList<Cliente>();
+
+				Cliente cliente = new Cliente();
+
+				// Sacamos las columnas del RS
+				String userName = resultSet.getString("userName");
+				String contrasenna = resultSet.getString("contrasenna");
+
+				// Metemos los datos a Ejemplo
+				cliente.setUserName(userName);
+				cliente.setContrasenna(contrasenna);
+
+				// Lo guardamos en ret
+				ret.add(cliente);
+			}
+		} catch (SQLException sqle) {
+			System.out.println("Error con la BBDD - " + sqle.getMessage());
+		} catch (Exception e) {
+			System.out.println("Error generico - " + e.getMessage());
+		} finally {
+			// Cerramos al reves de como las abrimos
+			try {
+				if (resultSet != null)
+					resultSet.close();
+			} catch (Exception e) {
+				// No hace falta
+			}
+			;
+			try {
+				if (statement != null)
+					statement.close();
+			} catch (Exception e) {
+				// No hace falta
+			}
+			;
+			try {
+				if (connection != null)
+					connection.close();
+			} catch (Exception e) {
+				// No hace falta
+			}
+			;
+		}
+		return ret;
 	}
 }
